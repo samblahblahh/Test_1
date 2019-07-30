6066d65f046c66191231f38895aa6d3cae243a92
Ubuntu <samraj94@gmail.com>
2019-07-30 17:20:10 +0000

    Second Git project

---
+++ javaprog.java
@@ -0,0 +1,12 @@
+class test{
+ public static void main (string [] args) {
+ system.out.println("Hello Moto.... TEST TEST TEST 123");
+
+ var numnOne= 1;
+ var numbTwo= 2;
+
+ system.out.println ("This is number: ",numnOne + "this is number: ", numbTwo);
+
+
+}
+}
